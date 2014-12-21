772fbbd333ffce4aac84e19e54ee1e400de5e2e0
Or Elimelech <ore@wix.com>
2014-12-21 15:46:39 +0200

    Changed test name

--- stock_test.go
+++ stock_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestGetStock(t *testing.T) {
+func TestGetSymbol(t *testing.T) {
 	stock, _ := GetQuote("aapl")
 	if stock.GetSymbol() != "AAPL" {
 		t.Error("The stock symbol should be equal to aapl but equals to", stock.GetSymbol())
