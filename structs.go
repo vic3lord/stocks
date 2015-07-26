2f1375733d090f8a0b1c80038517b9ce35d4ffeb
Or Elimelech <0r3limelech@gmail.com>
2015-07-26 22:59:49 +0300

    Merged structs into one

--- structs.go
+++ structs.go
@@ -2,21 +2,18 @@ package stocks
 
 type Stock struct {
 	List struct {
-		Resources []Resource
-	}
-}
-
-type Resource struct {
-	Resource struct {
-		Fields Fields
-	}
-}
-type Fields struct {
-	Name    string `json:"name"`
-	Price   string `json:"price"`
-	Symbol  string `json:"symbol"`
-	Ts      string `json:"ts"`
-	Type    string `json:"type"`
-	UTCTime string `json:"utctime"`
-	Volume  string `json:"volume"`
+		Resources []struct {
+			Resource struct {
+				Fields struct {
+					Name    string `json:"name"`
+					Price   string `json:"price"`
+					Symbol  string `json:"symbol"`
+					Ts      string `json:"ts"`
+					Type    string `json:"type"`
+					UTCTime string `json:"utctime"`
+					Volume  string `json:"volume"`
+				} `json:"fields"`
+			} `json:"resource"`
+		} `json:"resources"`
+	} `json:"list"`
 }
