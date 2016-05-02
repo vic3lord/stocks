36c36e01739f07c86e6067a5822defb0c1152975
Or Elimelech <0r3limelech@gmail.com>
2016-05-02 15:08:26 +0300

    Changed testing to use Test funcs instead of examples and make use of test data table

--- stock.go
+++ stock.go
@@ -9,60 +9,58 @@ import (
 	"time"
 )
 
-const (
-	timeout = time.Duration(time.Second * 10)
-)
+const timeout = time.Duration(time.Second * 10)
 
-// get full stock details into a struct
+// GetQuote gets latest stock quote for a given symbol
 func GetQuote(symbol string) (Stock, error) {
-	// set http client timeout
 	client := http.Client{Timeout: timeout}
 
 	url := fmt.Sprintf("http://finance.yahoo.com/webservice/v1/symbols/%s/quote?format=json", symbol)
 	res, err := client.Get(url)
 	if err != nil {
-		return Stock{}, fmt.Errorf("Stocks cannot access yahoo finance API: %v", err)
+		return Stock{}, fmt.Errorf("stocks cannot access yahoo finance API: %v", err)
 	}
 	defer res.Body.Close()
 
 	content, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return Stock{}, fmt.Errorf("Stocks cannot read json body: %v", err)
+		return Stock{}, fmt.Errorf("stocks cannot read JSON body: %v", err)
 	}
 
 	var stock Stock
-
 	err = json.Unmarshal(content, &stock)
 	if err != nil {
-		return Stock{}, fmt.Errorf("Stocks cannot parse json data: %v", err)
+		return Stock{}, fmt.Errorf("stocks cannot parse JSON data: %v", err)
 	}
 	return stock, nil
 }
 
-// return the stock name
-func (stock Stock) GetName() string {
-	return stock.List.Resources[0].Resource.Fields.Name
+// GetName gets stock name
+func (s Stock) GetName() string {
+	return s.List.Resources[0].Resource.Fields.Name
 }
 
-// return the stock symbol
-func (stock Stock) GetSymbol() string {
-	return stock.List.Resources[0].Resource.Fields.Symbol
+// GetSymbol gets stock symbol
+func (s Stock) GetSymbol() string {
+	return s.List.Resources[0].Resource.Fields.Symbol
 }
 
-// return the stock price
-func (stock Stock) GetPrice() (float64, error) {
-	price, err := strconv.ParseFloat(stock.List.Resources[0].Resource.Fields.Price, 64)
+// GetPrice gets the current stock price
+func (s Stock) GetPrice() (float64, error) {
+	p, err := strconv.ParseFloat(s.List.Resources[0].Resource.Fields.Price, 64)
 	if err != nil {
-		return 1.0, fmt.Errorf("Stock price: %v", err)
+		return 1.0, fmt.Errorf("stock price: %v", err)
 	}
-	return price, nil
+
+	return p, nil
 }
 
-// implement String()
-func (stock Stock) String() string {
-	price, err := stock.GetPrice()
+// String implements String()
+func (s Stock) String() string {
+	p, err := s.GetPrice()
 	if err != nil {
 		fmt.Printf("Error getting price: %v", err)
 	}
-	return fmt.Sprintf("Name:\t%s\nSymbol:\t%s\nPrice:\t%f\n", stock.GetName(), stock.GetSymbol(), price)
+
+	return fmt.Sprintf("Name:\t%s\nSymbol:\t%s\nPrice:\t%f\n", s.GetName(), s.GetSymbol(), p)
 }
--- stock_test.go
+++ stock_test.go
@@ -1,21 +1,40 @@
 package stocks
 
-import "fmt"
+import "testing"
 
-func ExampleStock_GetSymbol() {
-	stock, err := GetQuote("aapl")
-	if err != nil {
-		fmt.Printf("Error getting quote: %v", err)
-	}
-	fmt.Println(stock.GetSymbol())
-	// Output: AAPL
+var testData = []struct {
+	in     string
+	name   string
+	symbol string
+}{
+	{"aapl", "Apple Inc.", "AAPL"},
+	{"goog", "Alphabet Inc.", "GOOG"},
 }
 
-func ExampleStock_GetName() {
-	stock, err := GetQuote("aapl")
-	if err != nil {
-		fmt.Printf("Error getting quote: %v", err)
+func TestGetSymbol(t *testing.T) {
+	for _, tt := range testData {
+		s, err := GetQuote(tt.in)
+		if err != nil {
+			t.Errorf("Error getting quote for %s: %v", tt.in, err)
+		}
+
+		got := s.GetSymbol()
+		if got != tt.symbol {
+			t.Errorf("GetSymbol(%q) = %s; want %s", tt.in, got, tt.symbol)
+		}
 	}
-	fmt.Println(stock.GetName())
-	// Output: Apple Inc.
+}
+
+func TestGetName(t *testing.T) {
+	for _, tt := range testData {
+		s, err := GetQuote(tt.in)
+		if err != nil {
+			t.Errorf("Error getting quote for %s: %v", tt.in, err)
+		}
+
+		got := s.GetName()
+		if got != tt.name {
+			t.Errorf("GetSymbol(%q) = %s; want %s", tt.in, got, tt.symbol)
+		}
+	}
 }
